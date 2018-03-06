74468e64b5e660db58f1cc01c45a8b7e1b6b8a08
renat.shakurov <renat.shakurov@compulink.com>
2018-03-06 17:56:10 +0300

    update size 0.6

--- library/src/main/java/com/prolificinteractive/materialcalendarview/CalendarPagerView.java
+++ library/src/main/java/com/prolificinteractive/materialcalendarview/CalendarPagerView.java
@@ -284,7 +284,7 @@ abstract class CalendarPagerView extends ViewGroup implements View.OnClickListen
             final View child = getChildAt(i);
 
             final int width = child.getMeasuredWidth() - 20;
-            final int height = child.getMeasuredHeight();
+            final int height = child.getMeasuredHeight() - 3;
 
             child.layout(childLeft, childTop, childLeft + width, childTop + height);
 
