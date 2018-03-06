de0f3d6997dd8e32ea212b6350220d8fd20382d8
renat.shakurov <renat.shakurov@compulink.com>
2018-03-06 17:33:41 +0300

    update size 0.3

--- library/src/main/java/com/prolificinteractive/materialcalendarview/MaterialCalendarView.java
+++ library/src/main/java/com/prolificinteractive/materialcalendarview/MaterialCalendarView.java
@@ -1607,7 +1607,7 @@ public class MaterialCalendarView extends ViewGroup {
 
         //We need to disregard padding for a while. This will be added back later
         final int desiredWidth = specWidthSize - getPaddingLeft() - getPaddingRight();
-        final int desiredHeight = specHeightSize - getPaddingTop() - getPaddingBottom() - 90;
+        final int desiredHeight = specHeightSize - getPaddingTop() - getPaddingBottom();
 
         final int weekCount = getWeekCountBasedOnMode();
 
